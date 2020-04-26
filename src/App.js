4b086d788981133114cd32b90c0e6c5f0e307ca9
tonky0110 <tonky0110@gmail.com>
2020-04-26 22:51:34 +0900

    0.6 nesting(styled-component 안에서 다른 component를 선택해서 css를 적용하는 방법)

--- src/App.js
+++ src/App.js
@@ -12,19 +12,24 @@ const App = () => {
     </ThemeProvider>
   );
 }
-const Container = styled.div`
-  height: 100vh;
-  width: 100%;
-  background-color: #bdc3c7;
-`;
-const Card = styled.div`
-  background-color: red;
-`;
 
 const Button = styled.button`
   border-radius: 30px;
   padding: 25px 15px;
   background-color: ${props => props.theme.successColor}
+`;
+
+const Card = styled.div`
+  background-color: red;
+`;
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  background-color: #bdc3c7;
+  ${Card} {
+    background-color: blue;
+  }
 `;
 
 const Form = () => {
