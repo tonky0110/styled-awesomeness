7d4226e29abe1d57d1cbc9523fa2c30070abd611
tonky0110 <tonky0110@gmail.com>
2020-04-26 22:48:21 +0900

    0.5 theme provider적용

--- src/App.js
+++ src/App.js
@@ -1,37 +1,37 @@
 import React, { Fragment } from 'react';
 import 'reset-css';
-import styled, { css, keyframes } from 'styled-components';
+import styled, { ThemeProvider } from 'styled-components';
+import theme from './theme';
 
 const App = () => {
   return (
-    <Container>
-      <Input
-        placeholder="Hello"
-      />
-    </Container>
+    <ThemeProvider theme={theme}>
+      <Container>
+        <Form />
+      </Container>
+    </ThemeProvider>
   );
 }
-
-// mixin
-const awesomeCard = css`
-  box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
-  background-color: white;
-  border-radius: 10px;
-  padding: 20px;
-`;
-
 const Container = styled.div`
   height: 100vh;
   width: 100%;
   background-color: #bdc3c7;
 `;
-
-const Input = styled.input
-  .attrs({
-    required: true
-  })`
-  border: none;
-  ${awesomeCard}
+const Card = styled.div`
+  background-color: red;
 `;
 
+const Button = styled.button`
+  border-radius: 30px;
+  padding: 25px 15px;
+  background-color: ${props => props.theme.successColor}
+`;
+
+const Form = () => {
+  return (
+    <Card>
+      <Button>Hello</Button>
+    </Card>
+  );
+}
 export default App;
