2f7f14afae346489272ebc480ba066c899eb83de
tonky0110 <tonky0110@gmail.com>
2020-04-26 22:20:27 +0900

    0.4.2 mixin을 이용한 style공유

--- src/App.js
+++ src/App.js
@@ -12,6 +12,13 @@ const App = () => {
   );
 }
 
+// mixin
+const awesomeCard = css`
+  box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
+  background-color: white;
+  border-radius: 10px;
+  padding: 20px;
+`;
 
 const Container = styled.div`
   height: 100vh;
@@ -23,7 +30,8 @@ const Input = styled.input
   .attrs({
     required: true
   })`
-  border-radius: 5px;
+  border: none;
+  ${awesomeCard}
 `;
 
 export default App;
