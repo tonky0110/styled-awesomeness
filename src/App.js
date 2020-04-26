8b974f06c81bf1e7637d611a5ba34d3ae2cdea4a
tonky0110 <tonky0110@gmail.com>
2020-04-26 20:40:15 +0900

    0.1 전통적인 css를 이용한 style적용

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Fragment } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Button>Success</Button>
+      <Button danger>Hello</Button>
+    </Fragment>
   );
 }
+
+const Button = ({ danger, children }) => (
+  <button className={danger ? 'button button--denger' : 'button button--success'}>{children}</button>
+);
 
 export default App;
