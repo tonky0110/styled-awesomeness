32dd05c2cad93aa24bdb279d5a75164994047090
tonky0110 <tonky0110@gmail.com>
2020-04-26 20:52:41 +0900

    0.2 first styled-component css(Button)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 import React, { Fragment } from 'react';
-import './App.css';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  background-color: #bdc3c7;
+`;
+const Button = styled.button`
+	border-radius: 50px;
+	padding: 5px;
+	min-width: 120px;
+	color: white;
+	font-weight: 600;
+	-webkit-appearance: none;
+  cursor: pointer;
+  &:active,
+  &:focus {
+    outline: none;
+  }
+  background-color: ${props => props.danger ? '#e74c3c' : '#2ecc71'};
+`;
+
 
 const App = () => {
   return (
-    <Fragment>
+    <Container>
       <Button>Success</Button>
       <Button danger>Hello</Button>
-    </Fragment>
+    </Container>
   );
 }
 
-const Button = ({ danger, children }) => (
-  <button className={danger ? 'button button--denger' : 'button button--success'}>{children}</button>
-);
 
 export default App;
